28e62e339b268c274d946d1ab2dd895053858c4c
ilovesecu <ilovesecu@gmail.com>
2025-03-13 23:20:48 +0900

    usePathname과 /explore페이지

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,3 +1,24 @@
-export default function Page(){
-    return null;
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import style from './explore.module.css';
+import Trend from "@/app/(afterLogin)/_component/Trend";
+
+export default function Page() {
+    return (
+        <main className={style.main}>
+            <div className={style.formZone}>
+                <SearchForm/>
+            </div>
+            <div className={style.trend}>
+                <h3>나를 위한 트렌드</h3>
+                <Trend/>
+                <Trend/>
+                <Trend/>
+                <Trend/>
+                <Trend/>
+                <Trend/>
+                <Trend/>
+                <Trend/>
+            </div>
+        </main>
+    );
 }
