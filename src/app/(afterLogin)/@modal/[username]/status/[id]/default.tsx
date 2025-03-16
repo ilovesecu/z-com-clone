fa527a70e8e8ca98a7fa6dd1c6e2fb4ac8a4f412
ilovesecu <ilovesecu@gmail.com>
2025-03-16 23:46:38 +0900

    Message 하기싫음 ㅠㅠ

---
+++ src/app/(afterLogin)/@modal/[username]/status/[id]/default.tsx
@@ -0,0 +1,5 @@
+export default function IdDefault(){
+    return (
+        <></>
+    )
+}
--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -5,11 +5,14 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/ko';
 import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
 import PostArticle from "@/app/(afterLogin)/_component/PostArticle";
-
+import {faker} from '@faker-js/faker';
 dayjs.locale('ko');
 dayjs.extend(relativeTime)
 
-export default function Post(){
+type Props = {
+    noImage?:boolean
+}
+export default function Post({noImage}:Props){
     const target = {
         postId: 1,
         User: {
@@ -19,8 +22,15 @@ export default function Post(){
         },
         content: '클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ',
         createdAt: new Date(),
-        Images: [],
+        Images: [] as any[],
     }
+
+    if(Math.random() > 0.5 && !noImage){
+        target.Images.push(
+            {imageId:1, link:faker.image.urlLoremFlickr()}
+        )
+    }
+
     return (
         <PostArticle post={target}>
             <div className={style.postWrapper}>
@@ -44,9 +54,14 @@ export default function Post(){
                     </div>
                     <div>{target.content}</div>
                     <div className={style.postImageSection}>
-
+                        {target.Images && target.Images.length > 0 && (
+                            <Link href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`}
+                            className={style.postImageSection}>
+                                <img src={target.Images[0]?.link} alt=""/>
+                            </Link>
+                        )}
                     </div>
-                    <ActionButtons />
+                    <ActionButtons white/>
                 </div>
             </div>
         </PostArticle>
